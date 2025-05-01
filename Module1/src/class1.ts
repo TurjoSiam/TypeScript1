feba0598ef4efdd78cf084b464c3ecff328c3594
TurjoSiam <t.siam24@gmail.com>
2025-05-01 13:29:32 +0600

    [ADDED]-object property and value added with type

--- Module1/src/class1.ts
+++ Module1/src/class1.ts
@@ -1,2 +1,8 @@
 const task: string = "doing hard work and keeping consistency";
 console.log(task);
+
+const sum = (a: number, b: number): number => {
+    return a + b;
+};
+
+console.log(sum(55, 57));
---
+++ Module1/src/class5.ts
@@ -0,0 +1,17 @@
+// object data type
+
+const admin: {
+    userName: string,
+    email: string,
+    isActive: boolean,
+    experience: number,
+    company: string,
+    Location: string
+} = {
+    userName: "Siam",
+    email: "siam@gmail.com",
+    isActive: true,
+    experience: 4,
+    company: "Console Loggers",
+    Location: "Chittagong"
+}
