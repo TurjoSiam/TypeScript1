341f96abeddcdbf3221b4f1a22f078b3e4c3d004
TurjoSiam <t.siam24@gmail.com>
2025-05-01 16:42:15 +0600

    [ADDED]-new method added

--- Module1/src/class6.ts
+++ Module1/src/class6.ts
@@ -8,3 +8,15 @@ const totalResult = (third: number, fourth: number): number => {
     return third + fourth
 }
 
+
+//method = object with a function in it
+
+const admin1 = {
+    userName: "Siam",
+    age: 29,
+    email: "siam@gmail.com",
+    newAge (laterAge: number): string {
+        return `admin ${this.userName} has the new age ${this.age + laterAge}`;
+    }
+}
+
