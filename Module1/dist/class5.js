27d1e13e1d3d7d715c7f46feba3d0158c63acb5d
TurjoSiam <t.siam24@gmail.com>
2025-05-01 16:25:37 +0600

    [ADDED]-normal and arrow function added

---
+++ Module1/dist/class5.js
@@ -0,0 +1,11 @@
+"use strict";
+// object data type
+const admin = {
+    userName: "Siam",
+    email: "siam@gmail.com",
+    isActive: true,
+    experience: 4,
+    company: "Console Loggers",
+    Location: "Chittagong"
+};
+// admin.Location = "Dhaka"; 
---
+++ Module1/src/class6.ts
@@ -0,0 +1,10 @@
+//normal function and arrow function
+
+function total(first: number, second: number): number {
+    return first + second;
+}
+
+const totalResult = (third: number, fourth: number): number => {
+    return third + fourth
+}
+
