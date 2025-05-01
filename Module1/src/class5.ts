63bcd3ade6617bf861485913616c24b5ea495448
TurjoSiam <t.siam24@gmail.com>
2025-05-01 13:35:19 +0600

    [ADDED]-literal, optional, readonly type added

--- Module1/src/class5.ts
+++ Module1/src/class5.ts
@@ -4,9 +4,9 @@ const admin: {
     userName: string,
     email: string,
     isActive: boolean,
-    experience: number,
-    company: string,
-    Location: string
+    experience?: number,  //optional type
+    company: "Console Loggers",  //console loggers is considered as a type
+    readonly Location: string   //can not change 'readonly' value. it is called 'literal' type
 } = {
     userName: "Siam",
     email: "siam@gmail.com",
@@ -15,3 +15,7 @@ const admin: {
     company: "Console Loggers",
     Location: "Chittagong"
 }
+
+
+
+// admin.Location = "Dhaka"; 
