db84b8baae6a723a57986c3f98276bcbaf02b2e6
TurjoSiam <t.siam24@gmail.com>
2025-05-04 21:00:12 +0600

    [PRACTICED]-typescript constrints practiced

---
+++ Milestone1/src/Module2/constrains.ts
@@ -0,0 +1,36 @@
+{
+
+    const assigningTitle = <T extends { user: string; age: number }>(value: T): {} => {
+        return {
+            ...value,
+            title: "web developer"
+        };
+    };
+
+    type AssigningTitle = {
+        user: string;
+        age: number;
+        isAvailable: boolean
+    };
+
+    const result = assigningTitle<AssigningTitle>(
+        {
+            user: "siam",
+            age: 29,
+            isAvailable: true
+        }
+    );
+
+    const result1 = assigningTitle<AssigningTitle>(
+        {
+            user: "hasasn",
+            age: 30,
+            isAvailable: false
+        }
+    )
+
+    console.log(result);
+
+
+
+}
