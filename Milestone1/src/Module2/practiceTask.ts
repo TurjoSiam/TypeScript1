664aa881399406edfd4323a9223c81b95e1aebaf
TurjoSiam <t.siam24@gmail.com>
2025-05-05 22:51:27 +0600

    [PRACTICED]-task 1 and task 2 practiced

---
+++ Milestone1/src/Module2/practiceTask.ts
@@ -0,0 +1,29 @@
+//task 1
+const task1: string = "Hello World, I will complete this course successfully and become a Next level Web Developer!"
+
+// console.log(task1);
+
+
+//task 2
+const task2 = <T>(value: T): T => {
+    return value;
+}
+
+type Obj = {
+    name: string;
+    age: number;
+    role?: "admin" | "user" | "guest"
+}
+
+const obj: Obj = {
+    name: "siam",
+    age: 29,
+    role: "admin"
+}
+
+console.log(task2<Obj>(obj));
+
+
+//task3
+
+
